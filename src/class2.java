3c9b05dc88b04c9cf47a122485b2f1ff4af8125c
suma-shetty <sumashetty103@gmail.com>
2025-03-17 23:26:45 +0000

    pattern programming practice

---
+++ src/class2.java
@@ -0,0 +1,24 @@
+public class class2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        int n = 6;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
