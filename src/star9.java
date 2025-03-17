62a0b293a4d46d03f38f9fedb3bfb7d05e703961
suma-shetty <sumashetty103@gmail.com>
2025-03-17 08:15:00 +0000

    pattern programming practice

--- src/star9.java
+++ src/star9.java
@@ -3,7 +3,7 @@ public class star9 {
         int rows = 5;
         for (int i = 1; i <= rows; i++) {
      for (int j = 1; j <= i; j++) {
-         System.out.print(i + " ");
+         System.out.print(j + " ");
      }
             System.out.println();
         }
